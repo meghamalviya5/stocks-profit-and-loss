191bfc82769cf0cdc88c56b7f340a9cdea947f01
meghamalviya5 <megha.malviya444@gmail.com>
2022-12-05 23:43:34 +0530

    feat: added stocks profit loss calculator

---
+++ stocks.js
@@ -0,0 +1,31 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var tellMeBtn = document.querySelector("#tell-me-btn");
+var outputBox = document.querySelector("#output");
+
+tellMeBtn.addEventListener("click", calculateProfitOrLoss);
+
+function calculateProfitOrLoss() {
+  var initialPriceValue = Number(initialPrice.value);
+  var stocksQuantityValue = Number(stocksQuantity.value);
+  var currentPriceValue = Number(currentPrice.value);
+  var message = "";
+  if (currentPriceValue > initialPriceValue) {
+    var profit = (currentPriceValue - initialPriceValue) * stocksQuantityValue;
+    var profitPercentage = (profit / initialPriceValue) * 100;
+    message = `Hey the profit is ${profit} and the percentage is ${profitPercentage}%`;
+  } else if (currentPriceValue < initialPriceValue) {
+    var loss = (initialPriceValue - currentPriceValue) * stocksQuantityValue;
+    var lossPercentage = (loss / initialPriceValue) * 100;
+    message = `Hey the loss is ${loss} and the percentage is ${lossPercentage}$`;
+  } else {
+    message = "No pain no gain and no gain no pain";
+  }
+  console.log(message);
+  showMessage(message);
+}
+
+function showMessage(message) {
+  outputBox.innerText = message;
+}
